09e64a4cf4c14a72e8cc76d0a6b9cd9b5dea1814
itay-melman <154412815+itay-melman@users.noreply.github.com>
2023-12-25 13:02:52 +0200

    Update can_interface.py

--- app/can_interface.py
+++ app/can_interface.py
@@ -1,5 +1,20 @@
-# can_interface.py
+#can_interface.py
 import can
+import threading
+
+class WheelPositionEnum:
+    FRONT_LEFT = 1
+    FRONT_RIGHT = 2
+    REAR_LEFT = 3
+    REAR_RIGHT = 4
+
+# Dictionary to map sensor IDs to wheel positions
+SENSOR_ID_TO_WHEEL_POSITION = {
+    WheelPositionEnum.FRONT_LEFT: "front_left",
+    WheelPositionEnum.FRONT_RIGHT: "front_right",
+    WheelPositionEnum.REAR_LEFT: "rear_left",
+    WheelPositionEnum.REAR_RIGHT: "rear_right",
+}
 
 sensor_data = {}
 
@@ -11,11 +26,15 @@ class CANInterface(threading.Thread):
         message = can.Message(arbitration_id=arbitration_id, data=data, extended_id=extended_id)
         self.bus.send(message)
 
+    def get_wheel_position_from_id(self, sensor_id):
+        return SENSOR_ID_TO_WHEEL_POSITION.get(sensor_id)
+
     def run(self):
         while True:
             message = self.bus.recv()
             if message.arbitration_id == 0x123:
-                data = message.data
-                sensor_data[data[0]] = data[1]
-            
-        
+                sensor_id, pressure_value = message.data[0], message.data[1]
+                wheel_position = self.get_wheel_position_from_id(sensor_id)
+
+                with sensor_data_lock:
+                    sensor_data[wheel_position] = pressure_value
