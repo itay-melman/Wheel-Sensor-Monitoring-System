5cc98bb64b0e61d8a28be59fc6d589bb9ddb095a
itay-melman <154412815+itay-melman@users.noreply.github.com>
2023-12-25 12:35:32 +0200

    adding logic to can_interface

--- app/can_interface.py
+++ app/can_interface.py
@@ -1,10 +1,21 @@
 # can_interface.py
 import can
 
-class CANInterface:
+sensor data = {}
+
+class CANInterface(threading.Thread):
     def __init__(self, channel='can0', bustype='socketcan'):
         self.bus = can.interface.Bus(channel=channel, bustype=bustype)
 
     def send_message(self, arbitration_id, data, extended_id=False):
         message = can.Message(arbitration_id=arbitration_id, data=data, extended_id=extended_id)
         self.bus.send(message)
+
+    def run(self):
+        while True:
+            message = self.bus.recv()
+            if message.arbitration_id == 0x123:
+                data = message.data
+                sensor_data[data[0]] = data[1]
+            
+        
--- app/sensors.py
+++ app/sensors.py
@@ -7,11 +7,12 @@ sensor_data = {"front_left": 0, "front_right": 0, "rear_left": 0, "rear_right": 
 sensor_data_lock = threading.Lock()
 
 class SensorSimulator(threading.Thread):
-    def __init__(self, wheel_position, simulation_file_path, simulation_interval):
+    def __init__(self, wheel_position, simulation_file_path, simulation_interval, id):
         super(SensorSimulator, self).__init__()
         self.wheel_position = wheel_position
         self.simulation_file_path = simulation_file_path
         self.simulation_interval = simulation_interval
+        self.id = id
         self.bus = can.interface.Bus(channel='can0', bustype='socketcan')
 
     def run(self):
@@ -23,7 +24,7 @@ class SensorSimulator(threading.Thread):
             with sensor_data_lock:
                 sensor_data[self.wheel_position] = pressure_value
 
-            message = can.Message(arbitration_id=0x123, data=[pressure_value, 0, 0, 0], extended_id=False)
+            message = can.Message(arbitration_id=0x123, data=[self.id,pressure_value, 0, 0, 0], extended_id=False)
             self.bus.send(message)
 
             time.sleep(self.simulation_interval)
